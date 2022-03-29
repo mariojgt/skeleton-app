666f4320879ca16339fcac9b51a5ac6a1cd57bca
mario tarosso <mariojgt2@gmail.com>
2022-03-29 22:56:56 +0100

    up

--- src/pages/pos/Index.vue
+++ src/pages/pos/Index.vue
@@ -3,26 +3,14 @@
     <div class="q-pa-md">
       <div class="row">
         <div class="col">
-          <div class="q-pa-md bg-grey-10 text-white">
-            <q-list dark bordered separator style="">
-              <q-item clickable v-ripple>
-                <q-item-section>Single line item</q-item-section>
-              </q-item>
-
-              <q-item clickable v-ripple>
-                <q-item-section>
-                  <q-item-label>Item with caption</q-item-label>
-                  <q-item-label caption>Caption</q-item-label>
-                </q-item-section>
-              </q-item>
-
-              <q-item clickable v-ripple>
-                <q-item-section>
-                  <q-item-label overline>OVERLINE</q-item-label>
-                  <q-item-label>Item with caption</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
+          <div class="q-pa-md q-gutter-sm">
+            <div class="q-pa-md">
+              <div class="row reverse">
+                <div class="col-md-4">
+                  <q-btn color="primary" label="Primary" dense />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div class="col">Third, but first</div>
